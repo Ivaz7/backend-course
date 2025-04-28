8176ad26557b48237fa62124cfe60fdcffa7d984
Ivaz <ivazreza@gmail.com>
2025-04-28 10:46:16 +0700

    first-project
    feat: respone GET request for routes '/' dan '/dashboard'

--- first-project/server.js
+++ first-project/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 8383;
+
+app.get('/', (req, res) => {
+  console.log("I hit a endpoint", req.method);
+  res.sendStatus(201);
+});
+
+app.get('/dashboard', (req, res) => {
+  console.log("dashboard endpoint", req.method);
+  res.send("Hello Word")
+});
 
 app.listen(PORT, () => {
   console.log(`Server has started on: ${PORT}`)
