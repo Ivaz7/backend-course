13c3ce6a78900b425a7e5e848f12a2d3900cccf8
Ivaz <ivazreza@gmail.com>
2025-04-28 13:25:52 +0700

    second-project
    feat: add front end just for example from course

--- second-project/src/server.ts
+++ second-project/src/server.ts
@@ -1,10 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// config for telling where the public
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running in: ${PORT}`)
